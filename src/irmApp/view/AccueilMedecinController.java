d5c66d1ad892830ea3373c66e754b0e06e34b5f5
gphy <gphy@b2-info1-03>
2019-04-09 10:17:27 +0200

    partie tech

--- src/irmApp/view/AccueilMedecinController.java
+++ src/irmApp/view/AccueilMedecinController.java
@@ -11,7 +11,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
-import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
--- src/irmApp/view/AccueilTechnicienController.java
+++ src/irmApp/view/AccueilTechnicienController.java
@@ -17,6 +17,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 
 /**
  * La classe AccueilTechnicienController permet de gerer la page d'accueil des techniciens.
@@ -27,6 +28,9 @@ import javafx.scene.Scene;
  * @author Laure Baaudoin & Marie Bogusz
  */
 public class AccueilTechnicienController implements Initializable {
+    //*************************Partie Accueil***********************************
+    @FXML
+    private TabPane tabpane;
     // TableView de la liste des patients
     @FXML
     private TableView<Patient> patientTable;
@@ -43,6 +47,18 @@ public class AccueilTechnicienController implements Initializable {
     //Pop up d'erreur si un patient n'est pas séléctionné avant de passer à la page d'ajout d'un examen
     private Stage dialogStage;
 
+    //*************************Partie Ajout Examen******************************
+    @FXML
+    private GridPane gridpane;    
+    //Champs du formulaire d'ajout d'un examen
+    @FXML
+    private DatePicker dateExamen;
+    @FXML
+    private TextField idMachine, volCrane, axeCrane, volTumeur, ttp, rcbv, mtt;
+    @FXML
+    private TextField rcbf, lac, idMedecin, naa_cho, cho_cr, lip_cr, naa_cr;
+    
+    //*************************Partie Connexion à la bdd************************
     // connexion à la base de données
     private ConnexionOracle maconnection = new ConnexionOracle();
     // créer une variable de la requête  
@@ -80,6 +96,8 @@ public class AccueilTechnicienController implements Initializable {
         statutColumn.setMaxWidth( 1f * Integer.MAX_VALUE * 15 );
         sexeColumn.setMaxWidth( 1f * Integer.MAX_VALUE * 5 );
         gradeColumn.setMaxWidth( 1f * Integer.MAX_VALUE * 5 );
+        
+        gridpane.setVisible(false);
     }   
     
     /**
@@ -161,14 +179,13 @@ public class AccueilTechnicienController implements Initializable {
     * @param event
     */ 
     @FXML
-    public void handleAjoutExamen(ActionEvent event) throws IOException {
+    public void handleAjout(ActionEvent event){
         Patient aPatient;
         aPatient = patientTable.getSelectionModel().getSelectedItem();
     
         if (aPatient != null) {
-            Parent root = FXMLLoader.load(getClass().getResource("AjoutExamen.fxml"));
-            Scene scene = (Scene) ((Node) event.getSource()).getScene();
-            scene.setRoot(root);
+            tabpane.setVisible(false);
+            gridpane.setVisible(true);
         } 
         else {
             Alert alert = new Alert(Alert.AlertType.WARNING);
@@ -180,4 +197,107 @@ public class AccueilTechnicienController implements Initializable {
             alert.showAndWait();
         }
     }
+    
+    //====================================================Partie Ajout Examen===============================
+    /**
+     * isInputExamenValid() permet de vérifier si les champs du formulaire ne sont pas vides.
+     * 
+     * @return boolean
+     */
+    private boolean isInputExamenValid() {
+        String errorMessage = "";
+        
+        if (dateExamen.getValue() == null) {
+            errorMessage += "Date invalide !\n";
+        } 
+        if (idMachine.getText() == null || idMachine.getText().length() == 0) {
+            errorMessage += "ID machine invalide !\n";
+        }
+        if (volCrane.getText() == null || volCrane.getText().length() == 0) {
+            errorMessage += "Volume du crâne invalide !\n";
+        }
+        if (axeCrane.getText() == null || axeCrane.getText().length() == 0) {
+            errorMessage += "Valeur maximum de l'axe du crâne invalide !\n";
+        }
+        if (volTumeur.getText() == null || volTumeur.getText().length() == 0) {
+            errorMessage += "Volume de la tumeur invalide !\n";
+        }
+        if (ttp.getText() == null || ttp.getText().length() == 0) {
+            errorMessage += "TTP invalide !\n";
+        }
+        if (rcbv.getText() == null || rcbv.getText().length() == 0) {
+            errorMessage += "rCBV invalide !\n";
+        }
+        if (mtt.getText() == null || mtt.getText().length() == 0) {
+            errorMessage += "MTT invalide !\n";
+        }
+        if (rcbf.getText() == null || rcbf.getText().length() == 0) {
+            errorMessage += "rCBF invalide !\n";
+        }
+        if (lac.getText() == null || lac.getText().length() == 0) {
+            errorMessage += "Lac invalide !\n";
+        }
+        if(idMedecin.getText() == null || idMedecin.getText().length() == 0){
+            errorMessage += "ID Médecin invalide !\n";
+        }
+        if (naa_cho.getText() == null || naa_cho.getText().length() == 0) {
+            errorMessage += "NAA/Cho invalide !\n";
+        }
+        if (cho_cr.getText() == null || cho_cr.getText().length() == 0) {
+            errorMessage += "Cho/Cr invalide !\n";
+        }
+        if (lip_cr.getText() == null || lip_cr.getText().length() == 0) {
+            errorMessage += "Lip/Cr invalide !\n";
+        }
+        if (naa_cr.getText() == null || naa_cr.getText().length() == 0) {
+            errorMessage += "NAA/Cr invalide !\n";
+        }
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(null);
+            alert.setTitle("Attention !");
+            alert.setHeaderText("Veuillez corriger.");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+
+            return false;
+        }
+    }
+    
+    /**
+     * handleAjoutExamen() permet d'ajouter l'examen remplie dans le fomrulaire 
+     * à un patient dans la base de données.
+     * 
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    private void handleAjoutExamen(ActionEvent event) throws IOException {
+        if (isInputExamenValid()) {
+            /*String requeteAjout = "Insert into Examen (idMachine, idPatient, idMedecin, dateExam,"
+                   + " volCrane, valMaxAxeCrane, volTumeur, Cho_Cr, Naa_Cr, Naa_Cho, lip_cr, mtt,"
+                   + " ttp, rcbv, rcbf, lac) values ("+idMachine+","+idPatient().getId()+","+idMedecin+","
+                   + dateExamen+","+volCrane+","+axeCrane+","+volTumeur+","+cho_cr+","+naa_cr+","
+                   + naa_cho+","+lip_cr+","+mtt+","+ttp+","+rcbv+","+rcbf+","+lac+");";
+            try{
+                stmt = maconnection.ObtenirConnection().createStatement();
+                stmt.executeQuery(requeteAjout);
+                //petit pop up
+                JOptionPane.showMessageDialog(null, "Enregistré avec succès");
+                System.out.println("Enregistré");
+                //Revenir à la page d'accueil technicien
+                Parent root = FXMLLoader.load(getClass().getResource("AccueilTechnicien.fxml"));
+                Scene scene = (Scene) ((Node) event.getSource()).getScene();
+                scene.setRoot(root);
+            }
+            catch(SQLException e){
+                System.out.println(e);
+                System.out.println("Non enregistré");
+            }*/
+        }
+    }     
 }
